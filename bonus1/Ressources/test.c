bf730e6c959e2508602e175daa10228c6c757bb1
ldedier <dedierludovic@gmail.com>
2019-12-24 19:21:33 +0100

    bonus 1 complete

--- bonus1/Ressources/test.c
+++ bonus1/Ressources/test.c
@@ -1,24 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   source.c                                           :+:      :+:    :+:   */
+/*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ldedier <ldedier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/12/24 16:02:07 by ldedier           #+#    #+#             */
-/*   Updated: 2019/12/24 16:02:07 by ldedier          ###   ########.fr       */
+/*   Created: 2019/12/24 19:20:28 by ldedier           #+#    #+#             */
+/*   Updated: 2019/12/24 19:20:28 by ldedier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int argc, char **argv)
-{
-	char stack[64];
+#include <stdio.h>
+#include <stdlib.h>
 
-	if ((*(int *)(&stack[60]) = atoi(argv[1])) > 9)
-		return (1);
-	int size = ( *(int *)(&stack[60]) * 4 );
-	memcpy(&stack[20], argv[2], size);
-	if (ret == 0x574f4c46)
-		execl("/bin/sh", "sh", 0);
+int main(int argc, char**argv)
+{
+	printf("%d\n", atoi(argv[1]));
+	printf("%u\n", atoi(argv[1]));
+	printf("%u\n", atoi(argv[1]) * 4);
 	return (0);
 }
